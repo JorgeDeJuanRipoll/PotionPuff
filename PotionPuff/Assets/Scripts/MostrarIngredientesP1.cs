5ffb9fbbdaac0bd1a2b5400ca71e0b777897bb52
Claudieta06 <claudiaaliagamar06@gmail.com>
2026-02-03 18:52:31 +0100

    Menús y Botones

---
+++ PotionPuff/Assets/Scripts/MostrarIngredientesP1.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class MostrarIngredientesP1 : MonoBehaviour
+{
+    public GameObject panelIngredientes;
+    public float tiempoVisible = 2f;
+
+    public void Mostrar()
+    {
+        StopAllCoroutines(); // por si se pulsa varias veces
+        StartCoroutine(MostrarTemporalmente());
+    }
+
+    IEnumerator MostrarTemporalmente()
+    {
+        panelIngredientes.SetActive(true);
+        yield return new WaitForSeconds(tiempoVisible);
+        panelIngredientes.SetActive(false);
+    }
+}

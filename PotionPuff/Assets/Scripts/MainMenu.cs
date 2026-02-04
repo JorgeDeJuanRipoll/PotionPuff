cf24c8889e1ad5366fc963053bfe63ef436c8e40
Claudieta06 <claudiaaliagamar06@gmail.com>
2026-02-04 09:15:56 +0100

    Escena MainMenu HECHA

---
+++ PotionPuff/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InfoMenu : MonoBehaviour
+{
+	// Estos métodos se asignan a los botones desde el Inspector
+
+	public void Play()
+	{
+		SceneManager.LoadScene("SampleScene");
+	}
+
+	public void Controls()
+	{
+		SceneManager.LoadScene("Controls");
+	}
+
+	public void Menu()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+}
